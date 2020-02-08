371b8a50a6576bd2ce19a0777aa784c9e2fe6b0c
laukikm <laukikm11@gmail.com>
2020-02-07 17:54:19 -0700

    Converted to ROS package format

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/laukik/Perception-projects/devel:$CMAKE_PREFIX_PATH"
+export PWD="/home/laukik/Perception-projects/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/laukik/Perception-projects/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/laukik/Perception-projects/src:$ROS_PACKAGE_PATH"
